af08adb58d77bde72c5a5eb83d35275e50e499c2
noe reyes <noe.reyes92@yahoo.com>
2022-02-04 13:02:38 -0600

    Step 1 - COMPLETED

--- src/main/java/com/demo/demotodo/controller/HelloController.java
+++ src/main/java/com/demo/demotodo/controller/HelloController.java
@@ -3,7 +3,7 @@ package com.demo.demotodo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//COMMIT MESSAGE: Step 1 - COMPLETED
 @RestController
 @RequestMapping(path="/api")
 public class HelloController {
